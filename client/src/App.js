5c04ffb90b3d46d24393786ba7e63554376162a0
IBTechRaj <raj_shk@rediffmail.com>
2021-04-02 13:51:45 +0530

    input form done

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,29 @@ import './App.css'
 import { useState } from 'react'
 import Axios from 'axios'
 
+const YesNo = ['Yes', 'No']
+const AmbulatoryAid = [
+  'Furniture',
+  'Crutches',
+  'Walker',
+  'Cane',
+  'None',
+  'Bed Rest',
+  'Wheel Chair',
+  'Nurse'
+]
+const MentalStatus = ['Forgets Limitations', 'Oriented to Own Ability']
+const Medication = ['Under Sedation/Anesthasia', 'None']
+
 function App() {
+  const today = new Date()
+  const entryTime =
+    today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds()
+  const entryDate =
+    today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate()
+  console.log('dt', entryTime, entryDate)
+  const [currentDate, setCurrentDate] = useState(entryDate)
+  const [currentTime, setCurrentTime] = useState(entryTime)
   const [secDiag, setSecDiag] = useState(0)
   const [ambuAid, setAmbuAid] = useState('')
   const [heparinLock, setHeparinLock] = useState(0)
@@ -12,24 +34,25 @@ function App() {
   const [physicalRestraint, setPhysicalRestraint] = useState(0)
 
   const [fluidRestraint, setFluidRestraint] = useState(0)
+  const [patientDetails, setPatientDetails] = useState([])
 
   const addPatient = () => {
     Axios.post('http://localhost:3001/create', {
       secDiag: secDiag,
       ambuAid: ambuAid,
       heparinLock: heparinLock,
-      medicalCondition: medicalCondition,
+      medicatnCondition: medicatnCondition,
       mentalStatus: mentalStatus,
       physicalRestraint: physicalRestraint,
       fluidRestraint: fluidRestraint
     }).then(() => {
-      setPatientList([
-        ...patientList,
+      setPatientDetails([
+        ...patientDetails,
         {
           secDiag: secDiag,
           ambuAid: ambuAid,
           heparinLock: heparinLock,
-          medicalCondition: medicalCondition,
+          medicatnCondition: medicatnCondition,
           mentalStatus: mentalStatus,
           physicalRestraint: physicalRestraint,
           fluidRestraint: fluidRestraint
@@ -37,78 +60,144 @@ function App() {
       ])
     })
   }
-
-  const getEmployees = () => {
-    Axios.get('http://localhost:3001/employees').then(response => {
-      setEmployeeList(response.data)
-    })
-  }
+  // const clearPatient = () => {}
+  // const getPatientDetails = () => {
+  //     Axios.get('http://localhost:3001/patients').then(response => {
+  //   setPatientDetails(response.data)
+  //     })
+  // }
 
   return (
     <div className='App'>
-      <div className='information'>
-        <label>Date:</label>
-        <input
-          type='text'
-          onChange={event => {
-            setName(event.target.value)
-          }}
-        />
-        <label>Time:</label>
-        <input
+      <div className='row row-col-12 p-5 m-5'>
+        <div className='information'>
+          <label>Date:</label>
+          <input
+            type='number'
+            // disabled={{ entryDate }}
+            onChange={event => {
+              setCurrentDate(entryDate)
+            }}
+          />
+          <label>Time:</label>
+          <input
+            type='string'
+            // disabled={{ entryTime }}
+            onChange={event => {
+              setCurrentTime(entryTime)
+            }}
+          />
+          <label>Secondary Diagnosis:</label>
+          <select
+            type='string'
+            onChange={event => {
+              setSecDiag(event.target.value)
+            }}
+          >
+            <option></option>
+            {YesNo.map(yesno => (
+              <option key={yesno} value={yesno}>
+                {yesno}
+              </option>
+            ))}
+          </select>
+          <label>Ambulatory Aid:</label>
+          <select
+            type='string'
+            onChange={event => {
+              setAmbuAid(event.target.value)
+            }}
+          >
+            {AmbulatoryAid.map(ambaid => (
+              <option key={AmbulatoryAid} value={AmbulatoryAid}>
+                {ambaid}
+              </option>
+            ))}
+          </select>
+          <label>Heparin Lock:</label>
+          <select
+            type='string'
+            onChange={event => {
+              setHeparinLock(event.target.value)
+            }}
+          >
+            <option></option>
+            {YesNo.map(yesno => (
+              <option key={yesno} value={yesno}>
+                {yesno}
+              </option>
+            ))}
+          </select>
+
+          <label>Mental Status:</label>
+          <select
+            type='string'
+            onChange={event => {
+              setMentalStatus(event.target.value)
+            }}
+          >
+            <option></option>
+            {MentalStatus.map(ment => (
+              <option key={ment} value={ment}>
+                {ment}
+              </option>
+            ))}
+          </select>
+          <label>Medication Condition:</label>
+          <select
+            type='string'
+            onChange={event => {
+              setMedicatnCondition(event.target.value)
+            }}
+          >
+            <option></option>
+            {Medication.map(medic => (
+              <option key={medic} value={medic}>
+                {medic}
+              </option>
+            ))}
+          </select>
+          <label>Physical Restraint:</label>
+          <select
+            name='physicalRestraint'
+            onChange={event => {
+              setPhysicalRestraint(event.target.value)
+            }}
+          >
+            <option></option>
+            {YesNo.map(yesno => (
+              <option key={yesno} value={yesno}>
+                {yesno}
+              </option>
+            ))}
+          </select>
+          {/* <input
           type='number'
           onChange={event => {
-            setAge(event.target.value)
+            setPhysicalRestraint(event.target.value)
           }}
-        />
-        <label>Secondary Diagnosis:</label>
-        <input
-          type='text'
-          onChange={event => {
-            setCountry(event.target.value)
-          }}
-        />
-        <label>Ambulatory Aid:</label>
-        <input
-          type='text'
-          onChange={event => {
-            setPosition(event.target.value)
-          }}
-        />
-        <label>Heparin Lock:</label>
-        <input
-          type='number'
-          onChange={event => {
-            setWage(event.target.value)
-          }}
-        />
-        <label>Mental Status:</label>
-        <input
-          type='number'
-          onChange={event => {
-            setWage(event.target.value)
-          }}
-        />
-        <label>Heparin Lock:</label>
-        <input
-          type='number'
-          onChange={event => {
-            setWage(event.target.value)
-          }}
-        />
-        <label>Heparin Lock:</label>
-        <input
-          type='number'
-          onChange={event => {
-            setWage(event.target.value)
-          }}
-        />
-        <button onClick={addEmployee}>Add Employee</button>
+        /> */}
+          <label>Fluid Restraint:</label>
+          <select
+            type='string'
+            onChange={event => {
+              setFluidRestraint(event.target.value)
+            }}
+          >
+            <option></option>
+            {YesNo.map(yesno => (
+              <option key={yesno} value={yesno}>
+                {yesno}
+              </option>
+            ))}
+          </select>
+          <button onClick={addPatient}>Submit</button>
+        </div>
       </div>
       <div className='employees'>
-        <button onClick={getEmployees}>Show Employees</button>
+        {/* <button onClick={clearPatient}>Reset</button> */}
 
-        {employeeList.map((val, key) => {
+        {patientDetails.map((val, key) => {
           return (
             <div className='employee'>
               <div>
@@ -118,15 +207,15 @@ function App() {
                 <h3>Position: {val.position}</h3>
                 <h3>Wage: {val.wage}</h3>
               </div>
-              <div>
+              {/* <div>
                 <input
                   type='text'
                   placeholder='2000...'
                   onChange={event => {
                     setNewWage(event.target.value)
                   }}
-                />
-                <button
+                /> */}
+              {/* <button
                   onClick={() => {
                     updateEmployeeWage(val.id)
                   }}
@@ -141,8 +230,8 @@ function App() {
                   }}
                 >
                   Delete
-                </button>
-              </div>
+                </button> */}
+              {/* </div> */}
             </div>
           )
         })}
